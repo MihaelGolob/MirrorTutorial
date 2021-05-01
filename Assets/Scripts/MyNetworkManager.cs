3423545b54049c2d36206c1489bc1364b3e2c9d9
Mihael Golob <golob.mihael@gmail.com>
2021-05-01 17:26:27 +0200

    inital commit

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager {
+    // Some overrides of the callbacks in the network manager
+    public override void OnStartServer() {
+        base.OnStartServer();
+        Debug.Log("Server started");
+    }
+
+    public override void OnStopServer() {
+        base.OnStopServer();
+        Debug.Log("Server stopped");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn) {
+        base.OnClientConnect(conn);
+        Debug.Log("Connected to server");
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn) {
+        base.OnClientConnect(conn);
+        Debug.Log("Disconnected from server");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerController : NetworkBehaviour {
+    // variables synced between server and client
+    [SyncVar(hook = nameof(OnHelloCountChange))] private int helloCount = 0;
+    
+    private void HandleMovement() {
+        // Check if the script is running on the local player (we cant control other players)
+        if (isLocalPlayer) {
+            float moveHorizontal = Input.GetAxis("Horizontal") * 0.1f;
+            float moveVertical = Input.GetAxis("Vertical") * 0.1f;
+
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f);
+            transform.position += movement;
+        }
+    }
+
+    private void Update() {
+        HandleMovement();
+
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.X)) {
+            Debug.Log("Sending Hello message to server");
+            Hello();
+        }
+    }
+
+    public override void OnStartServer() {
+        base.OnStartServer();
+        Debug.Log("Player has been spawned on the server");
+    }
+
+    [Command]
+    private void Hello() {
+        Debug.Log("Received Hello from CLIENT");
+        helloCount++;
+        ReplyHello();
+    }
+
+    [TargetRpc]
+    private void ReplyHello() {
+        Debug.Log("Received Hello from SERVER");
+    }
+
+    [ClientRpc]
+    private void TooHigh() {
+        Debug.Log("Player too high!");
+    }
+    
+    void OnHelloCountChange(int oldc, int newc) {
+        Debug.Log($"We had {oldc} hellos, now we have {newc} hellos");
+    }
+}
